862b101bff7abb093785f65789e69613ef7d98ff
dmalishev86 <demetrious@bk.ru>
2022-08-01 17:48:57 +0300

    + наследование и отрицательные проверки

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -4,7 +4,7 @@ from .locators import BasketPageLocators
 class BasketPage(BasePage):
     def should_not_be_product_in_the_basket(self):
         assert self.is_not_element_present(*BasketPageLocators.PRODUCT_IN_THE_BASKET), \
-            "Product is presented, but should not be"
+            "Product is presented in the basket, but should not be"
 
     def should_be_empty_basket_message(self):
         assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET_MESSAGE), \
